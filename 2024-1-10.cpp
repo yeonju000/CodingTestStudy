1e2e35193d6ebdc2a2ce4af37de49e561fb4536b
galim03 <133737933+galim03@users.noreply.github.com>
2024-01-23 10:27:12 +0900

    Update 2024-1-10.cpp

--- 2024-1-10.cpp
+++ 2024-1-10.cpp
@@ -6,26 +6,14 @@ using namespace std;
 
 string solution(vector<int> food) {
     string answer = "";
-    if(2<=food.size()<=9){
-        int* count = new int[food.size()];
-        count[0]=food[0];
-        int arrange = 0;
-        for(int i=1; i<=food.size(); i++){
-            if(1<=food[i]<=1000){
-                count[i] = food[i]/2;
-                arrange += food[i]/2;
-            }
+    string second="";
+    for(int i=1; i<food.size(); i++){
+        for(int j=0; j<food[i]/2; j++){
+            answer+=to_string(i);
+            second.insert(0, to_string(i));
         }
-        string arr="";
-        
-        for(int j=0; j<food.size(); j++){
-            for(int k=0; k< count[j+1]; k++){
-                answer+=to_string(j+1);
-                arr.insert(0, to_string(j+1));
-            }
-        }
-        cout << answer;
-        answer.append('0'+ arr);
     }
+    second.insert(0, to_string(0));
+    answer+=second;
     return answer;
 }
